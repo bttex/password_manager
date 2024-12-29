b388ef6805557230dcc14a19aadec92c7e21f1b6
Bruno Teixeira <itbttex@outlook.com>
2024-12-29 11:59:53 -0300

    added hostname to ALLOWED_HOSTS

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['password-manager-5qc7.onrender.com',
+                 'localhost',
+                 '127.0.0.1',]
 
 # ...código existente...
 
