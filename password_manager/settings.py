e73b72a3fcd4c3f1ac5509381fc56242ce56c7d1
Bruno Teixeira <itbttex@outlook.com>
2024-12-29 12:22:47 -0300

    adiciona configuração para arquivos estáticos e cria arquivo de configuração do PostCSS

--- password_manager/settings.py
+++ password_manager/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
